57ee408a3669cb46a8d13e870f2e2149d31cd959
Joonas Yliaho <joonas.yliaho@gmail.com>
2018-09-19 14:16:44 +0300

    base for content page

---
+++ pages/content.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="content">
+    <aside>
+      Side Stuff
+    </aside>
+    <main>
+      
+    </main>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+</style>
