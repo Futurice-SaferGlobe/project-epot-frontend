9744a05f6c7a2fa2e0a513d5face3a68bd4c7fee
Joonas Yliaho <joonas.yliaho@gmail.com>
2018-09-19 14:34:40 +0300

    break arrow buttons on `OperationExplorer` into a separate component

--- components/OperationExplorer.vue
+++ components/OperationExplorer.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="operation-explorer">
     <div class="wrapper">
-      <button @click="eb" class="arrow-wrapper">
-        <svg class="arrow arrow-left" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 67 105" width="1rem">
-            <path fill-rule="nonzero" d="M35.18 105L.356 52.982 35.825 0H66.11l.693.464-35.159 52.518L66.468 105H35.18z"/>
-        </svg>
-      </button>
+      <span @click="eb">
+        <arrow angle="left"/>
+      </span>
       <div class="info">
         <h1 class="operation-name">{{name}}</h1>
         <span class="area">{{area}}</span>
       </div>
-      <button @click="eb" class="arrow-wrapper">
-        <svg class="arrow arrow-right" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 67 105" width="1rem">
-            <path fill-rule="nonzero" d="M35.18 105L.356 52.982 35.825 0H66.11l.693.464-35.159 52.518L66.468 105H35.18z"/>
-        </svg>
-      </button>
+      <span @click="eb">
+        <arrow angle="right"/>
+      </span>
     </div>
   </div>
 </template>
 
 <script>
+import Arrow from './OperationExplorer/Arrow'
+
 export default {
+  components: {
+    Arrow
+  },
+
   props: {
     name: String,
     area: String
@@ -30,6 +32,7 @@ export default {
 
   methods: {
     eb() {
+      console.log('hei')
       this.provider.canvas.changeActiveOperation()
     }
   },
@@ -71,51 +74,6 @@ export default {
         font-size: 0.8rem;
         color: #148fe5;
         font-weight: bold;
-      }
-    }
-
-    .arrow-wrapper {
-      outline: none;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background-color: whitesmoke;
-      padding: 1rem 1.5rem;
-      cursor: pointer;
-
-      .arrow {
-        transition: all 0.15s ease-out;
-        transform-origin: 50%;
-
-        path {
-          fill: rgba(black, 0.7);
-        }
-
-        &.arrow-right {
-          transform: rotate(180deg);
-        }
-      }
-
-      &:hover {
-        background-color: #148fe5;
-
-        .arrow {
-          path {
-            fill: white;
-          }
-
-          &.arrow-right {
-            transform: rotate(180deg) translateX(-5px);
-          }
-
-          &.arrow-left {
-            transform: translateX(-5px);
-          }
-        }
-      }
-
-      &:active {
-        background-color: darken(#148fe5, 10%);
       }
     }
   }
---
+++ components/OperationExplorer/Arrow.vue
@@ -0,0 +1,64 @@
+<template functional>
+    <button class="arrow-wrapper">
+      <svg :class="`arrow arrow-${props.angle}`" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 67 105" width="1rem">
+        <path fill-rule="nonzero" d="M35.18 105L.356 52.982 35.825 0H66.11l.693.464-35.159 52.518L66.468 105H35.18z"/>
+      </svg>
+    </button>
+</template>
+
+<script>
+export default {
+  functional: true,
+
+  props: {
+    angle: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.arrow-wrapper {
+  outline: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: whitesmoke;
+  padding: 1rem 1.5rem;
+  cursor: pointer;
+
+  .arrow {
+    transition: all 0.15s ease-out;
+    transform-origin: 50%;
+
+    path {
+      fill: rgba(black, 0.7);
+    }
+
+    &.arrow-right {
+      transform: rotate(180deg);
+    }
+  }
+
+  &:hover {
+    background-color: #148fe5;
+
+    .arrow {
+      path {
+        fill: white;
+      }
+
+      &.arrow-right {
+        transform: rotate(180deg) translateX(-5px);
+      }
+
+      &.arrow-left {
+        transform: translateX(-5px);
+      }
+    }
+  }
+
+  &:active {
+    background-color: darken(#148fe5, 10%);
+  }
+}
+</style>
