8eadffef271e5dcacbae74c642b9d417078d3cd9
Joonas Yliaho <joonas.yliaho@gmail.com>
2018-11-08 15:14:08 +0200

    clean up

--- components/OperationVisual/generatePathCurve.js
+++ components/OperationVisual/generatePathCurve.js
@@ -24,29 +24,9 @@ export const generatePathCurve = ({ source, target, linear }) => {
     Math.max(source.x, target.x) - Math.min(source.x, target.x)
   )
 
-  console.table([
-    {
-      sourceX: source.x,
-      targetX: target.x,
-      distance: getPathAbsoluteDistance(0.5),
-      rotation: (source.x * 180) / Math.PI - 90,
-      rotation2: (target.x * 180) / Math.PI - 90
-    }
-  ])
-
-  if (
-    (source.x <= Math.PI / 2 || source.x >= Math.PI * 1.5) &&
-    (target.x <= Math.PI / 2 || target.x >= Math.PI * 1.5)
-  ) {
-    d.curve = {
-      x: getRadialPoint(Math.sin(getPathAbsoluteDistance(0.5)), source.y).x,
-      y: getRadialPoint(Math.sin(getPathAbsoluteDistance(0.5)), source.y).y
-    }
-  } else {
-    d.curve = {
-      x: getRadialPoint(getPathAbsoluteDistance(0.5), source.y).x,
-      y: getRadialPoint(getPathAbsoluteDistance(0.5), source.y).y
-    }
+  d.curve = {
+    x: getRadialPoint(Math.abs(getPathAbsoluteDistance(0.5)), source.y).x,
+    y: getRadialPoint(Math.abs(getPathAbsoluteDistance(0.5)), source.y).y
   }
 
   return d
