12b452ddf8d94ceb84ec8fd29c5b1da69bec3aa6
Joonas Yliaho <joonas.yliaho@gmail.com>
2018-11-26 09:37:56 +0200

    remove `cors-anywhere`

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,7 @@ module.exports = {
     baseURL:
       process.env.NODE_ENV === 'production' ||
       process.env.DEPLOY_ENV === 'gh-pages'
-        ? 'https://cors-anywhere.herokuapp.com/http://46.101.183.17:8080'
+        ? 'http://46.101.183.17:8080'
         : 'http://localhost:8080',
     errorHandler(error) {}
   },
@@ -71,7 +71,7 @@ module.exports = {
         httpEndpoint:
           process.env.NODE_ENV === 'production' ||
           process.env.DEPLOY_ENV === 'gh-pages'
-            ? 'https://cors-anywhere.herokuapp.com/http://46.101.183.17:8080'
+            ? 'http://46.101.183.17:8080'
             : 'http://localhost:8080/graphql'
       }
     }
