3914b92a58f0108717aa4611d05fef720e51500a
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2019-12-19 20:35:11 +0200

    Remove foul language from comments

--- middleware/pageJumpTransform.js
+++ middleware/pageJumpTransform.js
@@ -1,6 +1,6 @@
 export default function({ isDev, store, route }) {
   /**
-   * Bail out if we're on production and the process tries to run shit on server.
+   * Bail out if we're on production and the process tries to run on server.
    *
    * NOTICE: Remove the line below if we're NOT generating a static build
    * and want to use server stuff.
